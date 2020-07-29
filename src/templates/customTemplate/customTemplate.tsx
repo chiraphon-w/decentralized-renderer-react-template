26ea6cbd960dec2761c54332fed90f3442c50c07
Chiraphon W <55229406+apyoon-7@users.noreply.github.com>
2020-07-29 21:54:43 +0700

    john

--- src/templates/customTemplate/customTemplate.tsx
+++ src/templates/customTemplate/customTemplate.tsx
@@ -5,12 +5,11 @@ import { CustomTemplateCertificate } from "../sample";
 import { PrintWatermark } from "../../core/PrintWatermark";
 
 const containerStyle = css`
-  background-color: #ffffff;
-  color: #000000;
+  background-color: #324353;
+  color: #ffffff;
   padding: 15px;
   margin: auto;
-  width: 70%;
-  height: 100%;
+  width: 80%;
   text-align: center;
 `;
 
@@ -21,12 +20,8 @@ export const CustomTemplate: FunctionComponent<
     <div css={containerStyle} className={className} id="custom-template">
       <PrintWatermark />
       <h1>{document.name}</h1>
-      <div>This is to certify that</div>
+      <div>awarded to</div>
       <h2>{document.recipient.name}</h2>
-      <div>was admitted to the Drgree of</div>
-      <h3>{document.recipient.dname}</h3>
-      <div>in Computer Engineering</div>
-      <div>on the 30th day of July in the year 2020</div>
     </div>
   );
 };
